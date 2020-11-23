8f2410a136a028e27b77779835766fa8d7b139c5
yagukom <49666040+yagukom@users.noreply.github.com>
2020-11-23 12:48:46 +0800

    Update and rename 向文件输出字符 to 向文件输出字符.c

--- 向文件输出字符.c
+++ 向文件输出字符.c
@@ -1,20 +1,20 @@
 #include "stdio.h"
 #include "stdlib.h"
-#pragma warning(disable : 4996)
+#pragma warning(disable : 4996)//VS2019环境设置
 
 int main() 
 {
-	FILE *targetFilePoint;
+	FILE *targetFilePoint;//定义文件指针
 	char fileName[]="hanhanda.txt";
 	char inputContent;
-	targetFilePoint = fopen(fileName,"w");
+	targetFilePoint = fopen(fileName,"w");//使文件指针指向hanhanda.txt文件
 	printf("请输入准备储存到磁盘里的字符串，以#号结束");
 	inputContent = getchar();
 	while (inputContent!='#')
 	{
 		fputc(inputContent, targetFilePoint);
-		putchar(inputContent);
+		putchar(inputContent);//将刚才输入的字符输出到电脑屏幕上
 		inputContent = getchar();
 	}
-	fclose(targetFilePoint); 
+	fclose(targetFilePoint); //关闭文件
 }
